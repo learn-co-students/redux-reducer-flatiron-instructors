b7b3cad9787104ed92a36de617d4869d16cc848f
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-02-11 20:26:06 -0500

    Done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case "ADD_FRIEND":
+      return {...state, friends: [...state.friends, action.payload]};
+    case "REMOVE_FRIEND":
+      return {...state, friends: state.friends.filter( f => f.id !== action.payload)}
+    default:
+      return state;
+  }
 }
